1b2db029f36faebc0b651427bcc8ad8d03efaab7
Jenkins <jenkins@laCapitale.com>
2019-10-18 14:28:47 +0000

    first commit

---
+++ jenkinsfile.groovy
@@ -0,0 +1,37 @@
+def runStage( pNamespace, pTagImage ) {
+
+//===============================================
+//========= YOU CAN EDIT HERE :) ================
+//===============================================
+
+  //stage ('build jar') {
+  //  dir ('app') {
+  //    //
+  //  }
+  //}
+
+  //stage ('static code analysis') {
+  //  dir ('app') {
+  //    //
+  //  }
+  //}
+
+  //stage ('static security analysis') {
+  //  dir ('app') {
+  //    //
+  //  }
+  //}
+
+  //stage ('unit tests') {
+  //  dir ('app') {
+  //    //
+  //  }
+  //}
+
+//===============================================
+//========= DO NOT EDIT BELOW THIS BOX ==========
+//===============================================
+
+}
+
+return this
