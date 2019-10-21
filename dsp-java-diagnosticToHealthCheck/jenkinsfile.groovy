6956edff390d2fde30ac23fdbeee524000bccf69
ext11264 <ali.aliche@lacapitale.com>
2019-10-21 16:30:19 -0400

    La version de 16:30 du projet

---
+++ dsp-java-diagnosticToHealthCheck/jenkinsfile.groovy
@@ -0,0 +1,39 @@
+def runStage( pNamespace, pTagImage ) {
+
+//===============================================
+//========= YOU CAN EDIT HERE :) ================
+//===============================================
+
+    SONAR_QUBE=false
+
+    stage ('build jar') {
+        dir ('app') {
+            sh "./mvnw clean package spring-boot:repackage"
+        }
+    }
+
+    //stage ('static code analysis') {
+    //  dir ('app') {
+    //    //
+    //  }
+    //}
+
+    //stage ('static security analysis') {
+    //  dir ('app') {
+    //    //
+    //  }
+    //}
+
+    /*stage ('unit tests') {
+        dir ('app') {*/
+    //   sh('./mvnw test')
+    // }
+    // }
+
+//===============================================
+//========= DO NOT EDIT BELOW THIS BOX ==========
+//===============================================
+
+}
+
+return this
