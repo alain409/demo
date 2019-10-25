d6359efa137be3d57da3bd9826d1cb98feb77d95
ext11264 <ali.aliche@lacapitale.com>
2019-10-25 09:30:40 -0400

    La version du 9:30

--- src/main/java/com/lacapitale/corporatif/diagnostic/page/service/ServiceProperties.java
+++ src/main/java/com/lacapitale/corporatif/diagnostic/page/service/ServiceProperties.java
@@ -1,5 +1,6 @@
 package com.lacapitale.corporatif.diagnostic.page.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -13,7 +14,7 @@ import java.io.InputStream;
 @Component("pageProperties")
 public class ServiceProperties {
     //Méthode pour récupérer un fichier du répertoire resources
-    public String getFileJson(){
+  public String getFileJson(){
 
         Resource resource = new ClassPathResource("diagnosticpage.json");
         File file = null;
